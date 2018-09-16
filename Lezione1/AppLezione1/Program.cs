cc2a921278ef28eaec70e912ca14ac667e1e4288
Simone Cocchi <ilgrosso7516@gmail.com>
2018-09-16 12:17:26 +0200

    Ho effettuato una modifica

---
+++ Lezione1/AppLezione1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLezione1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 777777;
+            Console.WriteLine($"il valore di a è {a}");
+            short s = (short)a;
+            Console.WriteLine($"il valore di s è {s}");
+
+        }
+    }
+}
